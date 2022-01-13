8bdfed26d24a507a0cd1755e15ea2ba5d516669e
Chikistq <Evesecurity1>
2022-01-13 15:17:49 +0300

    блок Галереи исправления главного слайдера.

--- app/js/Dom.js
+++ app/js/Dom.js
@@ -29,10 +29,69 @@ export function $(selector) {
   return new Dom(selector)
 }
 
-// $.setBG = function (select, optId) {
-//   const selectId
-//   for (let i=1; selectId.)
-//   document.querySelectorAll(option)
-//
-//
-// }
+$.modal = function(eTarget) {
+
+
+
+
+
+  function creatModal() {
+    const modal = document.createElement('div')
+    modal.classList.add('gallery__modal')
+
+
+
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="gallery__modal-wrap flex">
+          <div class="gallery__modal-img"></div>
+          
+          <div class="gallery__modal-descr flex">
+            <h3 class="gallery__modal-author">
+              Казимир Малевич
+            </h3>
+            <p class="gallery__modal-name">
+              “Женщина с граблями”
+            </p>
+            <span class="gallery__modal-year">
+              1931-1932
+            </span>
+            <p class="gallery__modal-text">
+              Картина из второй серии крестьянского цикла работ Казимира Малевича. Художник принялся за её создание в 1930-1931 годах, после того, как первый цикл был утерян после Берлинской и Варшавской выставок в 1927 году.
+            </p>
+          </div>
+          <button class="gallery__modal-exit links">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.666641 15.3044L15.3333 0.000153846L16 0.695801L1.33331 16L0.666641 15.3044Z" fill="black"/>
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.666725 -0.000125032L15.3334 15.3041L14.6667 15.9998L5.76143e-05 0.695522L0.666725 -0.000125032Z" fill="black"/>
+            </svg>
+          </button>
+        </div>
+    `)
+    document.body.appendChild(modal)
+
+    const url = eTarget.target.lastElementChild.dataset.id
+    // const url = parseInt((eTarget.target.lastElementChild.getAttribute('src')).match(/\d+/))
+    /* eslint-disable-next-line no-useless-escape */
+    const u = 'img/gallery' + url + '.jpg'
+    modal.firstElementChild.firstElementChild.style.backgroundImage = `url("${u}")`
+
+    return modal
+  }
+  /* eslint-disable-next-line no-unused-vars */
+  const $modal = creatModal(eTarget)
+
+  return {
+    open() {
+      $modal.classList.add('active')
+    },
+    close() {
+      $modal.classList.remove('active')
+
+    },
+    destroy() {
+      $modal.remove()
+    }
+  }
+
+
+}
--- app/js/index.js
+++ app/js/index.js
@@ -6,27 +6,90 @@ import '@/style/media.scss'
 import '@/js/simplebar.min';
 import '@/style/simplebar.css'
 
-import Swiper from 'swiper'
-import 'swiper/css'
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 import Choices from 'choices.js'
 import {$} from './Dom'
 
+/* eslint-disable-next-line no-unused-vars */
+const elementClosest = require('element-closest');
 
 document.addEventListener('DOMContentLoaded', function() {
 
   /* header-slider */
   /* eslint-disable-next-line no-unused-vars */
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.header__slider_swiper', {
+    loop: true,
     effect: 'fade',
     fadeEffect: {
       crossFade: true,
       disableOnInteraction: false
     },
     autoplay: {
-      delay: 500,
+      delay: 8000,
     },
-    speed: 400,
+    speed: 4000,
+  });
+
+
+  /* eslint-disable-next-line no-unused-vars */
+  const swiperGallery = new Swiper('.swiper-gallery', {
+
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    mousewheel: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 9000,
+    },
+    breakpoints: {
+      // // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+        grid: {
+          fill: 'row',
+          rows: 1,
+        },
+      },
+      // // when window width is >= 320px
+      576: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+      },
+      // when window width is >= 480px
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 31,
+        slidesPerGroup: 2,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+      },
+      // when window width is >= 640px
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        slidesPerGroup: 3,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+      }
+    }
   });
 
   /* header-selects */
@@ -38,7 +101,8 @@ document.addEventListener('DOMContentLoaded', function() {
   /* eslint-disable-next-line no-unused-vars */
   const choices2 = new Choices('#select2', {
     searchEnabled: false,
-    itemSelectText: '',
+    itemSelectText: 'Импрессионизм',
+    placeholder: true,
   })
   /* eslint-disable-next-line no-unused-vars */
   const choices3 = new Choices('#select3', {
@@ -52,6 +116,12 @@ document.addEventListener('DOMContentLoaded', function() {
   })
   /* eslint-disable-next-line no-unused-vars */
   const choices5 = new Choices('#select5', {
+    searchEnabled: false,
+    itemSelectText: '',
+  })
+
+  /* eslint-disable-next-line no-unused-vars */
+  const choices6 = new Choices('.gallery__filter-select', {
     searchEnabled: false,
     itemSelectText: '',
   })
@@ -77,6 +147,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setTimeout(function() {
       document.addEventListener('click', function(e) {
+        console.log(e.target)
         if (e.target !== sInput) {
           $(sBtn).removeClass('display-none')
         }
@@ -94,6 +165,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+  /* gallery-modal */
+
+  document.querySelectorAll('.gallery__slider-img').forEach(el => {
+    el.addEventListener('click', function(event) {
+      const crtModal = $.modal(event)
+
+      if (event.target.closest('.gallery__slider-img')) {
+        crtModal.open()
+
+        const $modal = $('.gallery__modal')
+        const $exitBtn = $('.gallery__modal-exit')
+
+        $modal.on('click', function(e) {
+
+          if (e.target == $modal.$el || e.target == $exitBtn.$el || e.target == $exitBtn.$el.firstElementChild) {
+            crtModal.close()
+            setTimeout(() => {
+              crtModal.destroy()
+            }, 300)
+          }
+        })
+      }
+    })
+  })
+
+  //
+  // document.addEventListener('click', function(e) {
+  //   const crtModal = $.modal(e)
+  //
+  //   if (e.target.closest('.gallery__slider-img')) {
+  //     crtModal.open()
+  //
+  //     const $modal = $('.gallery__modal')
+  //     const $exitBtn = $('.gallery__modal-exit')
+  //
+  //     $modal.on('click', function(e) {
+  //
+  //       if (e.target == $modal.$el || e.target == $exitBtn.$el || e.target == $exitBtn.$el.firstElementChild) {
+  //         crtModal.close()
+  //         setTimeout(() => {
+  //           crtModal.destroy()
+  //         }, 300)
+  //       }
+  //     })
+  //   }
+  // })
+
+  /* gallery-modal end */
 
 
 
